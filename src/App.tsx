967e801961ae6bd39f0454f3a1e687777e2a8589
alexkinsey <alexanderkinsey@icloud.com>
2021-07-22 18:00:53 +0100

    login page page created

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Login from './components/Login';
+
+// CONTEXT
+import UserProvider from './context';
 
 // STYLES
 import { GlobalStyle } from './GlobalStyle';
@@ -16,15 +20,18 @@ import { GlobalStyle } from './GlobalStyle';
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/movie/:id" element={<Movie />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-      <GlobalStyle />
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <GlobalStyle />
+      </UserProvider>
     </Router>
   );
-}
+};
 
 export default App;
